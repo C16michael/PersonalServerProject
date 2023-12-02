decebf252dca4902cdb6283bcec1e9671964639f
Cristiano Michael <c16michael@gmail.com>
2023-12-02 13:04:50 -0600

    Cookie Sessions, Authentication added

---
+++ credentials.js
@@ -0,0 +1,9 @@
+const MONGODB = {
+    user: "ACS3909",
+    login: "3147571",
+    cluster: "cluster0.d5xckcq.mongodb.net"
+};
+
+module.exports = {
+    MONGODB: MONGODB
+};
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+    console.log(`Previewing file: ${filename}`);
